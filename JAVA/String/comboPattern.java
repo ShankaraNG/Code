5f75510bc6ff2519b73213c2cc9e5ccf606fbfdb
Shankara Narayana N G <shankarnarayana92@gmail.com>
2024-12-27 23:24:25 +0530

    Update comboPattern.java

--- JAVA/String/comboPattern.java
+++ JAVA/String/comboPattern.java
@@ -19,6 +19,7 @@ cba*/
 
 package practice;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -110,26 +111,21 @@ public class comboPattern {
 	}
 	
 	public ArrayList<String> backtrack(String[] s, int start, ArrayList<String> result) {
-		if (start == s.length) {
-			return result;
-		}
-		else {
-			for (int i = start; i < s.length; i++) {
-				swap(s, start, i);
-				
-				if(!result.contains(String.join("", s))) {
-					result.add(String.join("", s));
-				}
-				
-			}
-			
-			
-			return backtrack(s, start + 1, result);
-		}
+        if (start == s.length) {
+        	result.add(String.join("", s));
+            return result;
+        }
 
+        for (int i = start; i < s.length; i++) {
+            swap(s, start, i);
+            backtrack(s, start + 1, result);
+            swap(s, start, i); // Backtrack
+        }
+        
+        return result;
 	}
 
-	private void swap(String[] s, int i, int j) {
+	public void swap(String[] s, int i, int j) {
 		String temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
