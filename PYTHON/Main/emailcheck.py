b786a9475a6a0e52066ee4cf05995aa010cdcff0
Shankara Narayana N G <shankarnarayana92@gmail.com>
2025-01-03 21:26:26 +0530

    Create emailcheck.py

---
+++ PYTHON/Main/emailcheck.py
@@ -0,0 +1,14 @@
+import re
+
+def search(a):
+    email_pattern = "^\w+([\.-]?\w+)*@\w+([|.-]?\w+)*(\?\w{2,3})+$"
+    result=re.search(email_pattern,a)
+    if result:
+        print("True")
+    else:
+        print("False")
+
+
+search("hankara\gmail?com")
+
+from abc import abstractmethod
